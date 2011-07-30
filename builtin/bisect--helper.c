8d5d856edd7db323c775c0b1e6024337467cbb2a
Jon Seymour <jon.seymour@gmail.com>
2011-07-31 03:05:39 +1000

    bisect: introduce support for --update-ref=<ref> option.
    If --update-ref=<ref> is specified, then the bisection process uses:
    
    	git update-ref --no-deref <ref> <trial>
    
    at each trial instead of:
    
    	git checkout <trial>
    
    Improved-by: Christian Couder <chriscool@tuxfamily.org>
    Signed-off-by: Jon Seymour <jon.seymour@gmail.com>

--- builtin/bisect--helper.c
+++ builtin/bisect--helper.c
@@ -4,16 +4,19 @@
 #include "bisect.h"
 
 static const char * const git_bisect_helper_usage[] = {
-	"git bisect--helper --next-all",
+	"git bisect--helper --next-all [--update-ref=<ref>]",
 	NULL
 };
 
 int cmd_bisect__helper(int argc, const char **argv, const char *prefix)
 {
 	int next_all = 0;
+	char *update_ref = NULL;
 	struct option options[] = {
 		OPT_BOOLEAN(0, "next-all", &next_all,
 			    "perform 'git bisect next'"),
+		OPT_STRING(0, "update-ref", &update_ref, "ref",
+			    "update <ref> instead of checking out the current commit"),
 		OPT_END()
 	};
 
@@ -24,5 +27,5 @@ int cmd_bisect__helper(int argc, const char **argv, const char *prefix)
 		usage_with_options(git_bisect_helper_usage, options);
 
 	/* next-all */
-	return bisect_next_all(prefix);
+	return bisect_next_all(prefix, update_ref);
 }
