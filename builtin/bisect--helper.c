2577ed8aa8645925b013f04228c7563345896b8c
Jon Seymour <jon.seymour@gmail.com>
2011-08-02 08:05:45 +1000

    bisect: introduce support for --no-checkout option.
    If --no-checkout is specified, then the bisection process uses:
    
    	git update-ref --no-deref HEAD <trial>
    
    at each trial instead of:
    
    	git checkout <trial>
    
    Improved-by: Christian Couder <chriscool@tuxfamily.org>
    Signed-off-by: Jon Seymour <jon.seymour@gmail.com>

--- builtin/bisect--helper.c
+++ builtin/bisect--helper.c
@@ -4,16 +4,20 @@
 #include "bisect.h"
 
 static const char * const git_bisect_helper_usage[] = {
-	"git bisect--helper --next-all",
+	"git bisect--helper --next-all [--bisect-mode=checkout|update-ref]",
 	NULL
 };
 
 int cmd_bisect__helper(int argc, const char **argv, const char *prefix)
 {
 	int next_all = 0;
+	int no_checkout = 0;
+	char *bisect_mode=NULL;
 	struct option options[] = {
 		OPT_BOOLEAN(0, "next-all", &next_all,
 			    "perform 'git bisect next'"),
+		OPT_STRING(0, "bisect-mode", &bisect_mode, "mode",
+			    "the bisection mode either checkout or update-ref. defaults to checkout."),
 		OPT_END()
 	};
 
@@ -23,6 +27,7 @@ int cmd_bisect__helper(int argc, const char **argv, const char *prefix)
 	if (!next_all)
 		usage_with_options(git_bisect_helper_usage, options);
 
+	no_checkout = bisect_mode && !strcmp(bisect_mode, "update-ref");
 	/* next-all */
-	return bisect_next_all(prefix);
+	return bisect_next_all(prefix, no_checkout);
 }
